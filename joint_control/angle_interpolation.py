abf1b72457e091c6d6d2ddae16d1a7c98fb70e73
Schwoisser <milan298@gmx.de>
2019-05-26 14:20:49 +0200

    Update angle_interpolation.py

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -21,7 +21,7 @@
 
 
 from pid import PIDAgent
-from keyframes import hello
+from keyframes import hello, leftBackToStand, leftBellyToStand,rightBackToStand, rightBellyToStand
 
 
 class AngleInterpolationAgent(PIDAgent):
@@ -32,6 +32,7 @@ class AngleInterpolationAgent(PIDAgent):
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.keyframes = ([], [], [])
+        self.start_time = False
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
@@ -41,10 +42,35 @@ class AngleInterpolationAgent(PIDAgent):
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
         # YOUR CODE HERE
+        # names, times, keys = keyframes
+        if not self.start_time:
+            self.start_time = perception.time
+        names, times, keys = keyframes
+        iterator = 0
+        for name in names:
+            time = times[iterator]
+            key = keys[iterator]
 
+            if name not in self.joint_names:
+                continue
+
+            dt = perception.time - self.start_time
+            for j in range(len(time) - 1):
+                if dt < time[0] and j == 0:
+                    t0, t3, p0, p3 = 0.0, time[0], perception.joint[name], key[0][0]
+                elif time[j] < dt < time[j+1]:
+                    t0, t3, p0, p3 = time[j], time[j + 1], key[j][0], key[j + 1][0]
+                else:
+                    t0, t3, p0, p3 = None, None, None, None
+                if t0 == None or t3 == None or p0 == None or p3 == None:
+                    continue
+                i = (dt - t0) / (t3 - t0)
+                target_joints[name] = (1 - i) ** 3 * p0 + 3 * (1 - i) ** 2 * i * (key[j][1][1] + p0) + 3 * (1 - i) * i ** 2 * (key[j][2][1] + p3) + i ** 3 * p3
+
+            iterator = iterator + 1
         return target_joints
 
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent.keyframes = leftBackToStand()  # CHANGE DIFFERENT KEYFRAMES
     agent.run()
